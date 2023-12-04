46764e2eb6235b5f8e74b74fab8f8ed9a5343013
Kevin <kevinadikasaputragithub@gmail.com>
2023-12-04 20:23:56 +0700

    :test_tube: Fix: fixing bug cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: 'sshkrk',
+  projectId: 'xh3o7n',
 
   component: {
     devServer: {
       framework: 'next',
       bundler: 'webpack',
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
